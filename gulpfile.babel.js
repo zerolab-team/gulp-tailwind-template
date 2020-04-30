88e4ff0cfb9ff60cc60e518cf6e0523cd8ad9f78
Sergey <newbornfrontender@gmail.com>
2020-04-30 20:26:13 +0300

    feat(gulp config): add styles and scripts tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,23 @@
 import { parallel, series } from 'gulp';
 
+// Gulp tasks
 import styles from './config/tasks/styles';
+import scripts from './config/tasks/scripts';
 
-export const start = series(parallel(styles));
+// Rollup plugins
+// import styles from './config/plugins/styles';
+
+export const start = series(
+  parallel((cb) => {
+    styles({ from: 'src/index.css', to: 'dist' });
+    scripts({
+      input: 'src/index.js',
+      dir: 'dist',
+      plugins: [
+        // styles(),
+      ],
+    });
+
+    cb();
+  }),
+);
