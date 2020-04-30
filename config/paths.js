f828a656a6b7d2164b7068988c29e6bcedf3883b
Sergey <newbornfrontender@gmail.com>
2020-04-30 22:55:05 +0300

    feat(paths): add paths

---
+++ config/paths.js
@@ -0,0 +1,13 @@
+const src = 'src';
+const dest = 'dist';
+
+export const paths = {
+  styles: {
+    from: `${src}/index.css`,
+    to: dest,
+  },
+  scripts: {
+    from: `${src}/index.js`,
+    to: dest,
+  },
+};
