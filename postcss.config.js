9b11cd5fdf50ea0137f2fb0a3840dd2901f4167f
Sergey <newbornfrontender@gmail.com>
2020-04-30 23:23:49 +0300

    feat(styling): add and configure postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,10 @@
-module.exports = () => ({
-  plugins: {},
+module.exports = ({ env }) => ({
+  plugins: {
+    'postcss-use': {},
+    'postcss-import': {},
+    'postcss-preset-env': {
+      stage: 2,
+      autoprefixer: env === 'production',
+    },
+  },
 });
