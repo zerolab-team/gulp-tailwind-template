76fbd733a95506ef4ec77dbd115d498ec486fdc9
Sergey <newbornfrontender@gmail.com>
2020-04-30 23:13:10 +0300

    feat(styling): replace task to rollup plugin

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,29 @@
 import { parallel, series } from 'gulp';
 import { paths } from './config/paths';
+import { getEnv } from './config/helpers';
 
 // Gulp tasks
-import styles from './config/tasks/styles';
+// import styles from './config/tasks/styles';
 import scripts from './config/tasks/scripts';
 
 // Rollup plugins
-// ...
+import postcss from 'rollup-plugin-postcss';
+
+const { isDev, isProd } = getEnv();
 
 export default series(
   parallel((cb) => {
-    styles({ from: paths.styles.from, to: paths.styles.to });
+    // styles({ from: paths.styles.from, to: paths.styles.to });
     scripts({
       input: paths.scripts.from,
       dir: paths.scripts.to,
-      plugins: [],
+      plugins: [
+        postcss({
+          extract: true,
+          minimize: isProd,
+          sourceMap: isDev,
+        }),
+      ],
     });
 
     cb();
