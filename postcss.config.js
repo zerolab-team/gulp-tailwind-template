cea1e1076abbd8b7cf21a948a61c1ae00af4c4c9
Sergey <newbornfrontender@gmail.com>
2020-05-01 11:07:51 +0300

    feat(styles): add tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,16 @@ module.exports = ({ env }) => ({
   plugins: {
     'postcss-use': {},
     'postcss-import': {},
+    'postcss-nested': {},
     'postcss-preset-env': {
-      stage: 2,
+      // stage: 2,
       autoprefixer: env === 'production',
+      features: {
+        'nesting-rules': true,
+      },
     },
+    tailwindcss: {},
+    'webp-in-css/plugin': {},
+    'postcss-sort-media-queries': {},
   },
 });
