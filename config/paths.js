76b4b46a72addf41c725a1e254008b14586df312
Sergey <newbornfrontender@gmail.com>
2020-05-01 11:06:28 +0300

    feat(gulp): add markup task

--- config/paths.js
+++ config/paths.js
@@ -2,6 +2,10 @@ const src = 'src';
 const dest = 'dist';
 
 export const paths = {
+  markup: {
+    from: `${src}/*.html`,
+    to: dest,
+  },
   styles: {
     from: `${src}/index.css`,
     to: dest,
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,19 +5,23 @@ import { getEnv } from './config/helpers';
 // Gulp tasks
 // import styles from './config/tasks/styles';
 import scripts from './config/tasks/scripts';
+import markup from './config/tasks/markup';
 
 // Rollup plugins
 import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
 
 const { isDev, isProd } = getEnv();
 
 export default series(
   parallel((cb) => {
     // styles({ from: paths.styles.from, to: paths.styles.to });
+    markup({ from: paths.markup.from, to: paths.markup.to });
     scripts({
       input: paths.scripts.from,
       dir: paths.scripts.to,
       plugins: [
+        resolve(),
         postcss({
           extract: true,
           minimize: isProd,
