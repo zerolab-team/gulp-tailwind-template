bcbc319339d0e544298039c5400cf66510a7f47a
Sergey <newbornfrontender@gmail.com>
2020-05-01 15:16:31 +0300

    chore(gulp conf): add tasks to base

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,6 @@ import { markup } from './config/tasks/markup';
 import { styles } from './config/tasks/styles';
 import { scripts } from './config/tasks/scripts';
 
-const base = parallel(markup);
+const base = parallel(markup, styles, scripts);
 
 export default series(clear, base);
